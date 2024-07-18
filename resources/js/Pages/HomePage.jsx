827110ee6202718ca3a0a99e68073584175d4a73
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-18 18:24:40 +0800

    feat: import and use HeroSection in HomePage.

--- resources/js/Pages/HomePage.jsx
+++ resources/js/Pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import DesktopMenu from "@/Components/common/DesktopMenu";
+import HeroSection from "@/Components/sections/HeroSection";
 import AppLayout from "@/Layouts/AppLayout";
 
 export default function HomePage() {
   return (
     <AppLayout>
       <DesktopMenu />
+      <HeroSection />
     </AppLayout>
   );
 }
