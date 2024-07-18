500b047591faedae6d24887ea0caca3e5e86fa02
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-18 14:38:04 +0800

    feat: create and use MegaMenu components

---
+++ resources/js/Components/common/MegaMenu.jsx
@@ -0,0 +1,112 @@
+const MegaMenu = () => {
+  return (
+    <div className="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:grid-cols-3 md:px-6">
+      <ul aria-labelledby="mega-menu-full-dropdown-button">
+        <li>
+          <a
+            href="#"
+            className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
+          >
+            <div className="font-semibold">Online Stores</div>
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              Connect with third-party tools that you're already using.
+            </span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
+          >
+            <div className="font-semibold">Segmentation</div>
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              Connect with third-party tools that you're already using.
+            </span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
+          >
+            <div className="font-semibold">Marketing CRM</div>
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              Connect with third-party tools that you're already using.
+            </span>
+          </a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a
+            href="#"
+            className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
+          >
+            <div className="font-semibold">Online Stores</div>
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              Connect with third-party tools that you're already using.
+            </span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
+          >
+            <div className="font-semibold">Segmentation</div>
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              Connect with third-party tools that you're already using.
+            </span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
+          >
+            <div className="font-semibold">Marketing CRM</div>
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              Connect with third-party tools that you're already using.
+            </span>
+          </a>
+        </li>
+      </ul>
+      <ul className="hidden md:block">
+        <li>
+          <a
+            href="#"
+            className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
+          >
+            <div className="font-semibold">Audience Management</div>
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              Connect with third-party tools that you're already using.
+            </span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
+          >
+            <div className="font-semibold">Creative Tools</div>
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              Connect with third-party tools that you're already using.
+            </span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
+          >
+            <div className="font-semibold">Marketing Automation</div>
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              Connect with third-party tools that you're already using.
+            </span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default MegaMenu;
--- resources/js/Components/common/Menu.jsx
+++ resources/js/Components/common/Menu.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { FaAngleDown, FaAngleUp, FaBars } from "react-icons/fa";
 import logoImg from "../../../images/logo quwwata.png";
+import MegaMenu from "./MegaMenu";
 
 const Menu = () => {
   const [megaMenuOpen, setMegaMenuOpen] = useState(false);
@@ -81,113 +82,7 @@ const Menu = () => {
           megaMenuOpen ? "block" : "hidden"
         }`}
       >
-        <div className="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:grid-cols-3 md:px-6">
-          <ul aria-labelledby="mega-menu-full-dropdown-button">
-            <li>
-              <a
-                href="#"
-                className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
-              >
-                <div className="font-semibold">Online Stores</div>
-                <span className="text-sm text-gray-500 dark:text-gray-400">
-                  Connect with third-party tools that you're already using.
-                </span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
-              >
-                <div className="font-semibold">Segmentation</div>
-                <span className="text-sm text-gray-500 dark:text-gray-400">
-                  Connect with third-party tools that you're already using.
-                </span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
-              >
-                <div className="font-semibold">Marketing CRM</div>
-                <span className="text-sm text-gray-500 dark:text-gray-400">
-                  Connect with third-party tools that you're already using.
-                </span>
-              </a>
-            </li>
-          </ul>
-          <ul>
-            <li>
-              <a
-                href="#"
-                className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
-              >
-                <div className="font-semibold">Online Stores</div>
-                <span className="text-sm text-gray-500 dark:text-gray-400">
-                  Connect with third-party tools that you're already using.
-                </span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
-              >
-                <div className="font-semibold">Segmentation</div>
-                <span className="text-sm text-gray-500 dark:text-gray-400">
-                  Connect with third-party tools that you're already using.
-                </span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
-              >
-                <div className="font-semibold">Marketing CRM</div>
-                <span className="text-sm text-gray-500 dark:text-gray-400">
-                  Connect with third-party tools that you're already using.
-                </span>
-              </a>
-            </li>
-          </ul>
-          <ul className="hidden md:block">
-            <li>
-              <a
-                href="#"
-                className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
-              >
-                <div className="font-semibold">Audience Management</div>
-                <span className="text-sm text-gray-500 dark:text-gray-400">
-                  Connect with third-party tools that you're already using.
-                </span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
-              >
-                <div className="font-semibold">Creative Tools</div>
-                <span className="text-sm text-gray-500 dark:text-gray-400">
-                  Connect with third-party tools that you're already using.
-                </span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
-              >
-                <div className="font-semibold">Marketing Automation</div>
-                <span className="text-sm text-gray-500 dark:text-gray-400">
-                  Connect with third-party tools that you're already using.
-                </span>
-              </a>
-            </li>
-          </ul>
-        </div>
+        <MegaMenu />
       </div>
     </nav>
   );
