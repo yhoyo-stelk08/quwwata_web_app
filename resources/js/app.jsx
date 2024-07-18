5a41bd059e3b6481fe68f87f911d34e09e0a1190
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-18 09:47:38 +0800

    feat: configure flowbite and swiper.

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,21 +1,32 @@
-import './bootstrap';
-import '../css/app.css';
+import "../css/app.css";
+import "./bootstrap";
 
-import { createRoot } from 'react-dom/client';
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createInertiaApp } from "@inertiajs/react";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { createRoot } from "react-dom/client";
+// import flowbite
+import { initFlowbite } from "flowbite";
+// import function to register Swiper custom elements
+import { register } from "swiper/element/bundle";
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+register();
+initFlowbite();
+
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
-    setup({ el, App, props }) {
-        const root = createRoot(el);
+  title: (title) => `${title} - ${appName}`,
+  resolve: (name) =>
+    resolvePageComponent(
+      `./Pages/${name}.jsx`,
+      import.meta.glob("./Pages/**/*.jsx")
+    ),
+  setup({ el, App, props }) {
+    const root = createRoot(el);
 
-        root.render(<App {...props} />);
-    },
-    progress: {
-        color: '#4B5563',
-    },
+    root.render(<App {...props} />);
+  },
+  progress: {
+    color: "#4B5563",
+  },
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,30 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import forms from "@tailwindcss/forms";
+import defaultTheme from "tailwindcss/defaultTheme";
+
+const flowbite = require("flowbite-react/tailwind");
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.jsx',
-    ],
+  content: [
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    "./storage/framework/views/*.php",
+    "./resources/views/**/*.blade.php",
+    "./resources/js/**/*.jsx",
+    "./node_modules/flowbite/**/*.js",
+    flowbite.content(),
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+        inter: ["Inter", ...defaultTheme.fontFamily.serif],
+        quicksand: ["Quicksand", ...defaultTheme.fontFamily.serif],
+        pacifico: ["Pacifico", "cursive"],
+        roboto_condensed: ["Roboto Condensed", "sans-serif"],
+      },
     },
+  },
 
-    plugins: [forms],
+  plugins: [flowbite.plugin(), forms, require("flowbite/plugin")],
 };
