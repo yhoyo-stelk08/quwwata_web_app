bf9f8ccbcf73e4be1f594c36750f92491e48e42f
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-18 14:03:27 +0800

    feat: finishing design menu components.

--- resources/js/Components/common/Menu.jsx
+++ resources/js/Components/common/Menu.jsx
@@ -1,30 +1,196 @@
-import { Link } from "@inertiajs/react";
-import { Navbar } from "flowbite-react";
+import { useState } from "react";
+import { FaAngleDown, FaAngleUp, FaBars } from "react-icons/fa";
+import logoImg from "../../../images/logo quwwata.png";
 
 const Menu = () => {
+  const [megaMenuOpen, setMegaMenuOpen] = useState(false);
+
+  const toggleMegaMenu = () => {
+    setMegaMenuOpen(!megaMenuOpen);
+  };
+
   return (
-    <Navbar fluid rounded>
-      <Navbar.Brand as={Link} href="/">
-        <img
-          src="/favicon.svg"
-          className="mr-3 h-6 sm:h-9"
-          alt="Quwwata Logo"
-        />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-          Quwwata Archery
-        </span>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse>
-        <Navbar.Link href="#">Home</Navbar.Link>
-        <Navbar.Link as={Link} href="#">
-          About
-        </Navbar.Link>
-        <Navbar.Link href="#">Gallery</Navbar.Link>
-        <Navbar.Link href="#">Pricing</Navbar.Link>
-        <Navbar.Link href="#">Contact</Navbar.Link>
-      </Navbar.Collapse>
-    </Navbar>
+    <nav className="bg-gradient-to-r from-slate-500 to-slate-950">
+      <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src={logoImg} className="h-14" alt="Flowbite Logo" />
+          <span className="self-center text-sm font-bold font-raleway whitespace-nowrap text-slate-200">
+            Quwwata <br /> Archery <br /> Shop
+          </span>
+        </a>
+        <button
+          data-collapse-toggle="mega-menu-full"
+          type="button"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="mega-menu-full"
+          aria-expanded="false"
+          // aria-expanded={megaMenuOpen ? "true" : "false"}
+          // onClick={toggleMegaMenu}
+        >
+          <span className="sr-only">Open main menu</span>
+          <FaBars className="h-6 w-6" />
+        </button>
+        <div
+          id="mega-menu-full"
+          className={`${
+            megaMenuOpen ? "block" : "hidden"
+          } items-center justify-between w-full md:flex md:w-auto md:order-1`}
+        >
+          <ul className="flex flex-col mt-4 font-medium md:flex-row md:mt-0 md:space-x-8 rtl:space-x-reverse ">
+            <li>
+              <a href="#" className="block menu-link" aria-current="page">
+                Home
+              </a>
+            </li>
+            <li>
+              <button
+                id="mega-menu-full-dropdown-button"
+                data-collapse-toggle="mega-menu-full-dropdown"
+                className="flex items-center justify-between w-full py-2 px-3 md:w-auto font-medium menu-link"
+                onClick={toggleMegaMenu}
+              >
+                Product{" "}
+                {megaMenuOpen ? (
+                  <FaAngleUp className="ml-1" />
+                ) : (
+                  <FaAngleDown className="ml-1" />
+                )}
+              </button>
+            </li>
+            <li>
+              <a href="#" className="block menu-link">
+                Marketplace
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block menu-link">
+                Resources
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block menu-link">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div
+        id="mega-menu-full-dropdown"
+        className={`mt-1 bg-white border-gray-200 shadow-sm border-y dark:bg-gray-800 dark:border-gray-600 ${
+          megaMenuOpen ? "block" : "hidden"
+        }`}
+      >
+        <div className="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:grid-cols-3 md:px-6">
+          <ul aria-labelledby="mega-menu-full-dropdown-button">
+            <li>
+              <a
+                href="#"
+                className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
+              >
+                <div className="font-semibold">Online Stores</div>
+                <span className="text-sm text-gray-500 dark:text-gray-400">
+                  Connect with third-party tools that you're already using.
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
+              >
+                <div className="font-semibold">Segmentation</div>
+                <span className="text-sm text-gray-500 dark:text-gray-400">
+                  Connect with third-party tools that you're already using.
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
+              >
+                <div className="font-semibold">Marketing CRM</div>
+                <span className="text-sm text-gray-500 dark:text-gray-400">
+                  Connect with third-party tools that you're already using.
+                </span>
+              </a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a
+                href="#"
+                className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
+              >
+                <div className="font-semibold">Online Stores</div>
+                <span className="text-sm text-gray-500 dark:text-gray-400">
+                  Connect with third-party tools that you're already using.
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
+              >
+                <div className="font-semibold">Segmentation</div>
+                <span className="text-sm text-gray-500 dark:text-gray-400">
+                  Connect with third-party tools that you're already using.
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
+              >
+                <div className="font-semibold">Marketing CRM</div>
+                <span className="text-sm text-gray-500 dark:text-gray-400">
+                  Connect with third-party tools that you're already using.
+                </span>
+              </a>
+            </li>
+          </ul>
+          <ul className="hidden md:block">
+            <li>
+              <a
+                href="#"
+                className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
+              >
+                <div className="font-semibold">Audience Management</div>
+                <span className="text-sm text-gray-500 dark:text-gray-400">
+                  Connect with third-party tools that you're already using.
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
+              >
+                <div className="font-semibold">Creative Tools</div>
+                <span className="text-sm text-gray-500 dark:text-gray-400">
+                  Connect with third-party tools that you're already using.
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
+              >
+                <div className="font-semibold">Marketing Automation</div>
+                <span className="text-sm text-gray-500 dark:text-gray-400">
+                  Connect with third-party tools that you're already using.
+                </span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
+
 export default Menu;
