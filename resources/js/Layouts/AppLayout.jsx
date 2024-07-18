ace97d18ff4ac840b7693742a984a98094dc3a50
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-18 19:56:40 +0800

    feat: move DesktopMenu components to AppLayout file.

--- resources/js/Layouts/AppLayout.jsx
+++ resources/js/Layouts/AppLayout.jsx
@@ -1,6 +1,9 @@
+import DesktopMenu from "@/Components/common/DesktopMenu";
+
 export default function AppLayout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
+      <DesktopMenu />
       <main className="flex-grow">{children}</main>
     </div>
   );
--- resources/js/Pages/HomePage.jsx
+++ resources/js/Pages/HomePage.jsx
@@ -1,11 +1,9 @@
-import DesktopMenu from "@/Components/common/DesktopMenu";
 import HeroSection from "@/Components/sections/HeroSection";
 import AppLayout from "@/Layouts/AppLayout";
 
 export default function HomePage() {
   return (
     <AppLayout>
-      <DesktopMenu />
       <HeroSection />
     </AppLayout>
   );
