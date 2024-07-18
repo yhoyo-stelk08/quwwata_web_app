d9e4962ad55184bea20ba5285d4845171f77ce21
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-18 15:43:21 +0800

    feat: rename the Menu into DesktopMenu.

--- resources/js/Components/common/DesktopMenu.jsx
+++ resources/js/Components/common/DesktopMenu.jsx
@@ -3,7 +3,7 @@ import { FaAngleDown, FaAngleUp, FaBars } from "react-icons/fa";
 import logoImg from "../../../images/logo quwwata.png";
 import MegaMenu from "./MegaMenu";
 
-const Menu = () => {
+const DesktopMenu = () => {
   const [megaMenuOpen, setMegaMenuOpen] = useState(false);
 
   const toggleMegaMenu = () => {
@@ -11,7 +11,7 @@ const Menu = () => {
   };
 
   return (
-    <nav className="bg-gradient-to-r from-slate-500 to-slate-950">
+    <nav className="bg-gradient-to-r from-slate-500 to-slate-950 relative">
       <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src={logoImg} className="h-14" alt="Flowbite Logo" />
@@ -24,9 +24,8 @@ const Menu = () => {
           type="button"
           className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="mega-menu-full"
-          aria-expanded="false"
-          // aria-expanded={megaMenuOpen ? "true" : "false"}
-          // onClick={toggleMegaMenu}
+          aria-expanded={megaMenuOpen ? "true" : "false"}
+          onClick={toggleMegaMenu}
         >
           <span className="sr-only">Open main menu</span>
           <FaBars className="h-6 w-6" />
@@ -37,13 +36,13 @@ const Menu = () => {
             megaMenuOpen ? "block" : "hidden"
           } items-center justify-between w-full md:flex md:w-auto md:order-1`}
         >
-          <ul className="flex flex-col mt-4 font-medium md:flex-row md:mt-0 md:space-x-8 rtl:space-x-reverse ">
+          <ul className="flex flex-col mt-4 font-medium md:flex-row md:mt-0 md:space-x-8 rtl:space-x-reverse">
             <li>
               <a href="#" className="block menu-link" aria-current="page">
                 Home
               </a>
             </li>
-            <li>
+            <li className="relative">
               <button
                 id="mega-menu-full-dropdown-button"
                 data-collapse-toggle="mega-menu-full-dropdown"
@@ -57,6 +56,7 @@ const Menu = () => {
                   <FaAngleDown className="ml-1" />
                 )}
               </button>
+              {megaMenuOpen && <MegaMenu />}
             </li>
             <li>
               <a href="#" className="block menu-link">
@@ -76,16 +76,8 @@ const Menu = () => {
           </ul>
         </div>
       </div>
-      <div
-        id="mega-menu-full-dropdown"
-        className={`mt-1 bg-white border-gray-200 shadow-sm border-y dark:bg-gray-800 dark:border-gray-600 ${
-          megaMenuOpen ? "block" : "hidden"
-        }`}
-      >
-        <MegaMenu />
-      </div>
     </nav>
   );
 };
 
-export default Menu;
+export default DesktopMenu;
--- resources/js/Pages/HomePage.jsx
+++ resources/js/Pages/HomePage.jsx
@@ -1,10 +1,10 @@
-import Menu from "@/Components/common/Menu";
+import DesktopMenu from "@/Components/common/DesktopMenu";
 import AppLayout from "@/Layouts/AppLayout";
 
 export default function HomePage() {
   return (
     <AppLayout>
-      <Menu />
+      <DesktopMenu />
     </AppLayout>
   );
 }
