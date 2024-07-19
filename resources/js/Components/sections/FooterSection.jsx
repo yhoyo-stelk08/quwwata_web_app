0d73a7ed7dad10feb6bc212c0929ad751134c6fd
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-19 11:03:16 +0800

    feat: create footer section.

---
+++ resources/js/Components/sections/FooterSection.jsx
@@ -0,0 +1,8 @@
+const FooterSection = () => {
+  return (
+    <section className="flex flex-col container w-full h-64">
+      FooterSection
+    </section>
+  );
+};
+export default FooterSection;
