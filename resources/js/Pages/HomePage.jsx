d7254a5b4c7641dadb0b2d95afe789073fc12d3c
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-20 19:53:23 +0800

    feat: import and use ProductCategories sections.

--- resources/js/Pages/HomePage.jsx
+++ resources/js/Pages/HomePage.jsx
@@ -1,6 +1,5 @@
-import CubeSlider from "@/Components/sections/CubeSlider";
 import HeroSection from "@/Components/sections/HeroSection";
-import LongParagraph from "@/Components/sections/LongParagraph";
+import ProductCategories from "@/Components/sections/ProductCategories";
 import TestimonySection from "@/Components/sections/TestimonySection";
 import AppLayout from "@/Layouts/AppLayout";
 
@@ -8,8 +7,7 @@ export default function HomePage() {
   return (
     <AppLayout>
       <HeroSection />
-      <CubeSlider />
-      <LongParagraph />
+      <ProductCategories />
       <TestimonySection />
     </AppLayout>
   );
