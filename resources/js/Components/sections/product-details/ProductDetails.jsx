c4754483fa98d111c9eb0411bdf105145a5851e9
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-22 22:47:22 +0800

    feat: re-layouting ProductDetails.

--- resources/js/Components/sections/product-details/ProductDetails.jsx
+++ resources/js/Components/sections/product-details/ProductDetails.jsx
@@ -1,28 +1,21 @@
 import ProductDetailsDescription from "./ProductDetailsDescription";
 import ProductDetailsGallery from "./ProductDetailsGallery";
 import ProductDetailsOptions from "./ProductDetailsOptions";
-import ProductDetailsOtherDescription from "./ProductDetailsOtherDescription";
 
 const ProductDetails = () => {
   return (
-    <div className="grid grid-rows-2 grid-cols-1">
-      <div className="flex flex-col lg:flex-row w-full h-full">
-        <div className="w-full xl:w-1/2 lg:w-1/2 h-full">
-          <ProductDetailsGallery />
-        </div>
-        <div className="w-full xl:w-1/2 lg:w-1/2 h-full">
-          <ProductDetailsOptions productType={"Laminated Bow"} />
-        </div>
+    <div className="grid grid-cols-1 lg:grid-cols-2 lg:grid-rows-2 xl:grid-cols-3 xl:grid-rows-1 gap-4">
+      <div className="col-span-1 row-span-1">
+        <ProductDetailsGallery />
       </div>
-      <div className="flex flex-col lg:flex-row w-full h-full">
-        <div className="w-full h-full border">
-          <ProductDetailsDescription />
-        </div>
-        <div className="w-full h-full border">
-          <ProductDetailsOtherDescription />
-        </div>
+      <div className="col-span-1 row-span-1">
+        <ProductDetailsOptions productType={"Laminated Bow"} />
+      </div>
+      <div className="lg:col-span-2 lg:row-span-1 xl:col-span-1 xl:row-span-1">
+        <ProductDetailsDescription />
       </div>
     </div>
   );
 };
+
 export default ProductDetails;
