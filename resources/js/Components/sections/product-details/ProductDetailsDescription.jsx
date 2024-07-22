59eb0489265f1243106b56019f71868e5a200171
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-22 23:09:57 +0800

    feat: adding margin top for each screen size.

--- resources/js/Components/sections/product-details/ProductDetailsDescription.jsx
+++ resources/js/Components/sections/product-details/ProductDetailsDescription.jsx
@@ -1,6 +1,6 @@
 const ProductDetailsDescription = () => {
   return (
-    <div className="flex flex-col w-full h-full p-2 text-slate-200 xl:py-8 md:py-8 md:space-y-6 text-sm xl:text-base lg:text-base md:text-base text-left xl:text-left lg:text-center tracking-tight font-inter">
+    <div className="flex flex-col w-full h-full p-2 text-slate-200 xl:py-8 md:py-8 md:space-y-6 text-sm xl:text-base lg:text-base md:text-base text-left xl:text-left lg:text-center tracking-tight font-inter xl:mt-6 lg:mt-0 md:mt-0 mt-0">
       <div className="py-4 px-6 border-b border-b-slate-400 border-opacity-60 lg:items-center">
         <h3 className=" hover:text-slate-50 w-full">Detail Description</h3>
       </div>
