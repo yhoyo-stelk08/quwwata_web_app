c163d1100c46473c0333c816ef5eebe25570a9aa
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-22 20:44:18 +0800

    feat: add link route to product details.

--- resources/js/Components/common/MegaMenuItem.jsx
+++ resources/js/Components/common/MegaMenuItem.jsx
@@ -1,15 +1,17 @@
+import { Link } from "@inertiajs/react";
+
 const MegaMenuItem = ({ title, description }) => {
   return (
     <li>
-      <a
-        href="#"
+      <Link
+        href={route("product-details")}
         className="block p-3 rounded-lg hover:bg-gradient-to-t hover:from-slate-500 hover:to-white transition-all duration-300 transform scale-100 hover:scale-110"
       >
         <div className="font-semibold text-slate-950">{title}</div>
         <span className="text-sm text-slate-900 text-opacity-50 dark:text-gray-400">
           {description}
         </span>
-      </a>
+      </Link>
     </li>
   );
 };
