85ec74568370569568c6162702a53cdd38672283
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-22 20:46:18 +0800

    feat: finishing ProductDetailsOptions design.

--- resources/js/Components/sections/product-details/ProductDetailsOptions.jsx
+++ resources/js/Components/sections/product-details/ProductDetailsOptions.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaCartArrowDown } from "react-icons/fa";
 
 const poundage = [
   "25 lbs",
@@ -10,13 +11,35 @@ const poundage = [
   "55 lbs",
 ];
 
+const customColor = ["Black", "Red", "Green", "White"];
+
 const ProductDetailsOptions = ({ productType }) => {
   const [drawWeight, setDrawWeight] = useState(null);
+  const [colorOption, setColorOption] = useState(null);
+  const [qty, setQty] = useState(1);
+
   const handleLbsClick = (poundage) => {
     setDrawWeight((prev) => poundage);
   };
+
+  const handleColorClick = (color) => {
+    setColorOption((prev) => color);
+  };
+
+  const handleQtyChange = (e) => {
+    setQty((prev) => e.target.value);
+  };
+
+  const incrementQty = () => {
+    setQty((prevQty) => prevQty + 1);
+  };
+
+  const decrementQty = () => {
+    setQty((prevQty) => Math.max(prevQty - 1, 0)); // prevent quantity from going below 0
+  };
+
   return (
-    <div className="flex flex-col border w-full h-full p-2 py-4 md:py-10 gap-2">
+    <div className="flex flex-col w-full h-full p-2 py-4 md:py-10 gap-2">
       <h1 className="font-medium font-roboto_condensed mt-4 text-4xl md:text-6xl lg:text-5xl xl:text-6xl tracking-tighter text-transparent bg-clip-text bg-gradient-to-r from-yellow-800 to-yellow-200">
         Turkish Laminated Bow
       </h1>
@@ -33,8 +56,9 @@ const ProductDetailsOptions = ({ productType }) => {
         <div className=" grid grid-cols-4 lg:grid-cols-5 pl-2 mt-4 gap-4">
           {poundage.map((lbs, index) => (
             <button
+              type="button"
               key={index}
-              className={`py-2 px-4 bg-slate-400 text-white rounded-lg ${
+              className={`py-2 px-4 bg-slate-400 text-white lg:text-sm rounded-lg ${
                 drawWeight === lbs ? "bg-slate-700" : ""
               }`}
               onClick={() => handleLbsClick(lbs)}
@@ -45,17 +69,67 @@ const ProductDetailsOptions = ({ productType }) => {
         </div>
       </div>
       <div>
-        <h3 className="pl-2 text-xl text-slate-200 mt-4">
+        <h3 className="pl-2  text-slate-200 mt-4">
           {productType === "Laminated Bow"
-            ? "Arrow Pass"
+            ? "Arrow Pass : "
             : productType === "Fiber Bow"
-            ? "Bow Color"
-            : "Vanes Color"}
+            ? "Bow Color : "
+            : "Vanes Color : "}
+          {colorOption}
         </h3>
+        <div className="grid grid-cols-2 md:grid-cols-4 auto-rows-auto gap-4 py-4 px-4">
+          {productType === "Laminated Bow" &&
+            customColor.map((color, index) => {
+              return (
+                <button
+                  type="button"
+                  key={index}
+                  onClick={() => handleColorClick(color)}
+                  className={`rounded-lg py-3 px-6 bg-${
+                    color === "Black"
+                      ? "black text-white"
+                      : color === "White"
+                      ? "white"
+                      : color.toLowerCase() + "-600 text-white"
+                  }`}
+                >
+                  {color}
+                </button>
+              );
+            })}
+        </div>
       </div>
-      <div className="flex gap-4">
-        <div>Quantity</div>
-        <div>Button Add To Cart</div>
+      <div className="flex gap-4 pl-2 mt-5 items-center md:items-start">
+        <div className="flex flex-col md:flex-row gap-2 items-center">
+          <h3 className=" text-slate-200 hidden md:block">Quantity : </h3>
+          <div className="flex items-center">
+            <button
+              onClick={decrementQty}
+              className="w-10 h-10 bg-slate-500 text-white rounded-l-lg flex justify-center items-center ml-2"
+            >
+              -
+            </button>
+            <input
+              type="text"
+              readOnly
+              value={qty}
+              onChange={handleQtyChange}
+              className="w-20 h-10 text-center bg-slate-400 bg-opacity-80 text-slate-50"
+            />
+            <button
+              onClick={incrementQty}
+              className="w-10 h-10 bg-slate-500 text-white rounded-r-lg flex justify-center items-center"
+            >
+              +
+            </button>
+          </div>
+        </div>
+        <div>
+          <button className="flex gap-2 text-white bg-gradient-to-br from-yellow-200 to-orange-600 items-center justify-center p-3 rounded-md">
+            <FaCartArrowDown className="w-6 h-6" />
+            Add To Cart
+          </button>
+        </div>
       </div>
     </div>
   );
