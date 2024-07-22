a27b5385c4b54308cd4311777dd1200dcbdeaa5d
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-22 19:34:48 +0800

    feat: add skeleton layout.

--- resources/js/Components/sections/product-details/ProductDetailsDescription.jsx
+++ resources/js/Components/sections/product-details/ProductDetailsDescription.jsx
@@ -1,4 +1,8 @@
 const ProductDetailsDescription = () => {
-  return <div>ProductDetailsDescription</div>;
+  return (
+    <div className="flex flex-col border w-full h-full p-2">
+      ProductDetailsDescription
+    </div>
+  );
 };
 export default ProductDetailsDescription;
