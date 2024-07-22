81495c012b3d4712028eadca7655e286df472637
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-22 08:16:36 +0800

    feat: import and use 4 new compoents.

--- resources/js/Components/sections/product-details/ProductDetails.jsx
+++ resources/js/Components/sections/product-details/ProductDetails.jsx
@@ -1,13 +1,26 @@
+import ProductDetailsDescription from "./ProductDetailsDescription";
+import ProductDetailsGallery from "./ProductDetailsGallery";
+import ProductDetailsOptions from "./ProductDetailsOptions";
+import ProductDetailsOtherDescription from "./ProductDetailsOtherDescription";
+
 const ProductDetails = () => {
   return (
     <>
-      <div className="flex flex-col md:flex-row w-full h-full border border-red-500">
-        <div className="w-full h-full border">Image Slider</div>
-        <div className="w-full h-full border">Product Custom Choice</div>
+      <div className="flex flex-col lg:flex-row w-full h-full">
+        <div className="w-full h-full border">
+          <ProductDetailsGallery />
+        </div>
+        <div className="w-full h-full border">
+          <ProductDetailsOptions />
+        </div>
       </div>
-      <div className="flex flex-col md:flex-row w-full h-full border border-red-500">
-        <div className="w-full h-full border">Product Description</div>
-        <div className="w-full h-full border">Other Detail / Optional</div>
+      <div className="flex flex-col lg:flex-row w-full h-full">
+        <div className="w-full h-full border">
+          <ProductDetailsDescription />
+        </div>
+        <div className="w-full h-full border">
+          <ProductDetailsOtherDescription />
+        </div>
       </div>
     </>
   );
