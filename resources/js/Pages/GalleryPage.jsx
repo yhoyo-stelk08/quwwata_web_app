2f4e7853817d6a5d2c855814381b78713a2967a1
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-23 18:36:20 +0800

    feat: add function filtering data fetched from server based on active tab.

--- resources/js/Pages/GalleryPage.jsx
+++ resources/js/Pages/GalleryPage.jsx
@@ -8,7 +8,9 @@ import { useEffect, useRef, useState } from "react";
 export default function GalleryPage() {
   const ref = useRef(null);
   const inView = useInView(ref, { once: true });
-  const [galleries, setGalleries] = useState([]);
+  const [allGalleries, setAllGalleries] = useState([]);
+  const [filteredGalleries, setFilteredGalleries] = useState([]);
+  const [activeTab, setActiveTab] = useState(0);
 
   const customTheme = {
     base: "flex flex-col gap-4",
@@ -69,16 +71,51 @@ export default function GalleryPage() {
   };
 
   useEffect(() => {
-    const galleryData = async () => {
+    const fetchGalleryData = async () => {
       try {
+        // fetching gallery data from backend
         const response = await axios.get("/all-gallery-data");
-        setGalleries(response.data);
+        setAllGalleries(response.data);
+        setFilteredGalleries(response.data); // Initialize with all galleries
       } catch (error) {
-        console.error("There was error when fetching gallery data: ", error);
+        console.error("There was an error when fetching gallery data: ", error);
       }
     };
-    galleryData();
+    fetchGalleryData();
   }, []);
+
+  useEffect(() => {
+    let category = "";
+    switch (activeTab) {
+      case 1:
+        category = "laminated bow";
+        break;
+      case 2:
+        category = "flat bow";
+        break;
+      case 3:
+        category = "arrows";
+        break;
+      case 4:
+        category = "accessories";
+        break;
+      default:
+        category = "";
+        break;
+    }
+
+    if (category) {
+      setFilteredGalleries(
+        allGalleries.filter((gallery) => gallery.category === category)
+      );
+    } else {
+      setFilteredGalleries(allGalleries);
+    }
+  }, [activeTab, allGalleries]);
+
+  const handleTabChange = (index) => {
+    setActiveTab(index);
+  };
 
   return (
     <AppLayout>
@@ -96,30 +133,43 @@ export default function GalleryPage() {
           </motion.h3>
         </div>
         {/* Gallery Tab */}
-        <div className="gap-2 mt-10 overflow-x-auto  whitespace-nowrap">
+        <div className="gap-2 mt-10 overflow-x-auto whitespace-nowrap">
           <Tabs
             aria-label="Tabs with underline"
             variant="underline"
-            className="gap-4 transition-all duration-300"
+            className="gap-4 transition-all duration-300 justify-start md:justify-center"
             theme={customTheme}
+            onActiveTabChange={handleTabChange}
           >
             <Tabs.Item title="All" className="inline-block">
               {/* Gallery Content */}
               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mt-10">
-                <Gallery galleries={galleries} />
+                <Gallery galleries={filteredGalleries} />
               </div>
             </Tabs.Item>
             <Tabs.Item className="inline-block" title="Laminated Bow">
-              Laminated Bow
+              {/* Gallery Content */}
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mt-10">
+                <Gallery galleries={filteredGalleries} />
+              </div>
             </Tabs.Item>
             <Tabs.Item className="inline-block" title="Fiber Flat Bow">
-              Fiber Flat Bow
+              {/* Gallery Content */}
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mt-10">
+                <Gallery galleries={filteredGalleries} />
+              </div>
             </Tabs.Item>
             <Tabs.Item className="inline-block" title="Arrow">
-              Arrow
+              {/* Gallery Content */}
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mt-10">
+                <Gallery galleries={filteredGalleries} />
+              </div>
             </Tabs.Item>
             <Tabs.Item className="inline-block" title="Accessories">
-              Accessories
+              {/* Gallery Content */}
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mt-10">
+                <Gallery galleries={filteredGalleries} />
+              </div>
             </Tabs.Item>
           </Tabs>
         </div>
