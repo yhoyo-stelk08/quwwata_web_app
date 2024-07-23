d919b3f87c73fa704176789aee4f29eae65203a9
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-23 10:47:44 +0800

    feat: populate GalleryIndex page.

--- resources/js/Pages/Gallery/Index.jsx
+++ resources/js/Pages/Gallery/Index.jsx
@@ -1,3 +1,28 @@
-export default function Index() {
-  return <div>Index</div>;
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Link } from "@inertiajs/react";
+
+export default function GalleryIndex({ galleriesData, auth }) {
+  return (
+    <AuthenticatedLayout
+      user={auth.user}
+      header={
+        <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+          Manage Gallery Items
+        </h2>
+      }
+    >
+      {/* <Head title="Dashboard" /> */}
+
+      <div className="py-12">
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div className="p-6 text-gray-900 dark:text-gray-100">
+              Gallery Index
+              <Link href={route("galleries.create")}>Add Images</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </AuthenticatedLayout>
+  );
 }
