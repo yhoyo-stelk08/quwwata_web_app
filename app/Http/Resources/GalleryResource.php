1d41fb772a4cfac10f7c61e5ae5fe681987a569b
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-26 14:25:49 +0800

    feat: use FormatGallery in GalleryResource.

--- app/Http/Resources/GalleryResource.php
+++ app/Http/Resources/GalleryResource.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Resources;
 
+use App\traits\FormatGallery;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class GalleryResource extends JsonResource
 {
+    use FormatGallery;
     /**
      * Transform the resource into an array.
      *
