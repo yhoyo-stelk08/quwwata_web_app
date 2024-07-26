efea8f7ad6dd6e4f4a14d4edaa81bc97a9b7632f
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-26 13:46:12 +0800

    feat: adding function formatDate to Gallery trait.

--- app/traits/FormatGallery.php
+++ app/traits/FormatGallery.php
@@ -11,8 +11,13 @@ trait FormatGallery
             'title' => $model->title,
             'image_name' => $model->image_name,
             'category' => $model->category,
-            'created_at' => $model->created_at->format('d M Y'),
-            'updated_at' => $model->updated_at->format('d M Y'),
+            'created_at' => $this->formatDate($model->created_at),
+            'updated_at' => $this->formatDate($model->updated_at),
         ];
+    }
+
+    private function formatDate($date)
+    {
+        return $date ? \Carbon\Carbon::parse($date)->format('Y-m-d H:i') : null;
     }
 }
