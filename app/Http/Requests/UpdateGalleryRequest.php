ec300708706b78f2d1c9a580a32565055b32a0c4
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-27 23:10:32 +0800

    feat: create new request to update gallery.

---
+++ app/Http/Requests/UpdateGalleryRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateGalleryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'category' => 'required|string|in:laminated bow,flat bow,arrows,accessories',
+            'image_name' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ];
+    }
+}
