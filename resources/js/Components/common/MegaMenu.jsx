c0c781cc36cd2f529f15a7e532b0b35f312983dc
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-28 09:31:28 +0800

    feat: re-arrange menu.

--- resources/js/Components/common/MegaMenu.jsx
+++ resources/js/Components/common/MegaMenu.jsx
@@ -26,42 +26,28 @@ const MegaMenu = ({ isOpen }) => {
       <div className="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:grid-cols-2 md:px-6 bg-gradient-to-t from-slate-500 to-white dark:bg-gray-800 border-gray-200 shadow-sm border-y dark:border-gray-600 absolute left-1/2 transform -translate-x-1/2 w-full sm:w-[400px] md:w-[600px] lg:w-[600px] xl:w-[800px] 2xl:w-[800px] mt-9 rounded-2xl opacity-100">
         <ul aria-labelledby="mega-menu-full-dropdown-button" className="mx-2">
           <MegaMenuItem
-            title={"Turkish Laminated Bow"}
+            title={" Laminated Bow"}
             description={
               "New laminated bow made with new materials, more flexible than the E-fiberglass. Very accurate and smooth to draw"
             }
           />
           <MegaMenuItem
-            title={"Tartar Laminated Bow"}
+            title={"Flat Fiber Bow"}
             description={
-              "New laminated bow made with new materials, more flexible than the E-fiberglass. Very accurate and smooth to draw"
-            }
-          />
-          <MegaMenuItem
-            title={"Cremean Laminated Bow"}
-            description={
-              "New laminated bow made with new materials, more flexible than the E-fiberglass. Very accurate and smooth to draw"
+              "New flat fiber bow made with new materials, more flexible than the E-fiberglass. Very accurate and smooth to draw"
             }
           />
         </ul>
         <ul className="mx-2">
           <MegaMenuItem
-            title={"Turkish Flat Fiber Bow"}
-            description={
-              "New flat fiber bow made with new materials, more flexible than the E-fiberglass. Very accurate and smooth to draw"
-            }
-          />
-          <MegaMenuItem
-            title={"Bow String"}
-            description={
-              "Custom Strings, we strive to provide top quality custom options for all your traditional longbow, compound, and recurve needs"
-            }
-          />
-          <MegaMenuItem
             title={"Arrow"}
             description={
               "A very straight bamboo with a large leaf. Makes a dense screen and is a slow spreading bamboo that prefers shady locations"
             }
+          />
+          <MegaMenuItem
+            title={"Accessories"}
+            description={"Custom Strings, Fletching Jig, and Other Accessories"}
           />
         </ul>
       </div>
