637402942698f3c41515597f77849ca4c0b1bf0b
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-28 10:41:59 +0800

    feat: add new props to ProductDetailsHeader. - Add new props to ProductDetailsHeader component. - Update the ProductDetailsHeader component to use the new props.
     Changes to be committed:
    	modified:   resources/js/Components/sections/product-details/ProductDetailsHeader.jsx
    	modified:   resources/js/Pages/ProductDetailsPage.jsx

--- resources/js/Components/sections/product-details/ProductDetailsHeader.jsx
+++ resources/js/Components/sections/product-details/ProductDetailsHeader.jsx
@@ -1,15 +1,28 @@
 import imgHeader1 from "../../../../images/detail-product/header1.jpeg";
+import imgHeader2 from "../../../../images/detail-product/header2.jpeg";
+import imgHeader3 from "../../../../images/detail-product/header3.jpeg";
+import imgHeader4 from "../../../../images/detail-product/header4.jpeg";
 
-const ProductDetailsHeader = () => {
+const ProductDetailsHeader = ({ title, category }) => {
+  let imgHeader = "";
+  if (category === "laminated-bow") {
+    imgHeader = imgHeader1;
+  } else if (category === "flat-bow") {
+    imgHeader = imgHeader2;
+  } else if (category === "arrows") {
+    imgHeader = imgHeader3;
+  } else {
+    imgHeader = imgHeader4;
+  }
   return (
     <div className="m-0 w-full h-full relative opacity-70">
       <img
-        src={imgHeader1}
+        src={imgHeader}
         alt=""
         className="object-cover w-full h-full absolute top-0 left-0"
       />
       <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-50 text-white p-2 rounded text-2xl md:text-4xl lg:text-5xl text-center font-pacifico">
-        Turkish Laminated Bow
+        {title}
       </div>
     </div>
   );
--- resources/js/Pages/ProductDetailsPage.jsx
+++ resources/js/Pages/ProductDetailsPage.jsx
@@ -2,11 +2,21 @@ import ProductDetails from "@/Components/sections/product-details/ProductDetails
 import ProductDetailsHeader from "@/Components/sections/product-details/ProductDetailsHeader";
 import AppLayout from "@/Layouts/AppLayout";
 
-const ProductDetailsPage = () => {
+const ProductDetailsPage = ({ category }) => {
+  let headerTitle = "";
+  if (category === "laminated-bow") {
+    headerTitle = "Laminated Bow";
+  } else if (category === "flat-bow") {
+    headerTitle = "Flat Fiber Bow";
+  } else if (category === "arrows") {
+    headerTitle = "Arrows";
+  } else {
+    headerTitle = "Bow Accessories";
+  }
   return (
     <AppLayout>
       <div className="flex flex-col w-full h-72 xl:h-96 md:h-[400px] mx-auto mt-0">
-        <ProductDetailsHeader />
+        <ProductDetailsHeader title={headerTitle} category={category} />
       </div>
       <div className="flex flex-col w-[90%] mx-auto my-20 mt-10 ">
         <ProductDetails />
