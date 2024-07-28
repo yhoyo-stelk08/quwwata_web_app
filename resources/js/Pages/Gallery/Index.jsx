f9670c1ca84bddd7d7ec6d5819c2421016830dc6
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-28 18:07:54 +0800

    feat: change the placeholder for search

--- resources/js/Pages/Gallery/Index.jsx
+++ resources/js/Pages/Gallery/Index.jsx
@@ -120,7 +120,7 @@ export default function GalleryIndex({
                   value={searchTerm}
                   type="text"
                   autoComplete="off"
-                  placeholder="Cari naskah masuk..."
+                  placeholder="Search Item..."
                   id="search"
                   className="block w-full rounded-lg border-0 py-2 pl-10 text-gray-900 ring-1 ring-inset ring-gray-200 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
