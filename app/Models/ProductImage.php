804e2d6cc6f236df82c7105adb45a7adf3f96ab7
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-28 14:15:03 +0800

    feat: define the relationship one-to-many.

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ProductImage extends Model
 {
     use HasFactory;
+
+    protected $table = 'product_images';
+    protected $guarded = [];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Products::class);
+    }
 }
--- app/Models/Products.php
+++ app/Models/Products.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Products extends Model
 {
@@ -11,4 +12,16 @@ class Products extends Model
 
     protected $table = 'products';
     protected $guarded = [];
+
+    public function product_images(): HasMany
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+    public function scopeSearch($query, $search)
+    {
+        return $query->where('name', 'like', '%' . $search . '%')
+            ->orWhere('short_description', 'like', '%' . $search . '%')
+            ->orWhere('long_description', 'like', '%' . $search . '%');
+    }
 }
