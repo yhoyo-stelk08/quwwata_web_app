7f85e147695dd6ad5706739c44a07b9b5b4d944a
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-28 11:08:08 +0800

    feat: modify the migration files for Products and ProductImage.

--- database/migrations/2024_07_28_025056_create_products_table.php
+++ database/migrations/2024_07_28_025056_create_products_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('price')->default(0);
+            $table->integer('draw_weight')->default(0);
+            $table->string('short_description');
+            $table->string('arrow_pass');
+            $table->text('long_description');
             $table->timestamps();
         });
     }
--- database/migrations/2024_07_28_025948_create_product_images_table.php
+++ database/migrations/2024_07_28_025948_create_product_images_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,9 @@ return new class extends Migration
     {
         Schema::create('product_images', function (Blueprint $table) {
             $table->id();
+            $table->string('image_name');
+            $table->string('path');
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
             $table->timestamps();
         });
     }
