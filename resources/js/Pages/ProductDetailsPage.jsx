06ed1a824df9322b5f9df07bfd082088a3fa088b
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-30 18:03:14 +0800

    feat: add fetch data from database function.

--- resources/js/Pages/ProductDetailsPage.jsx
+++ resources/js/Pages/ProductDetailsPage.jsx
@@ -1,22 +1,33 @@
 import ProductDetails from "@/Components/sections/product-details/ProductDetails";
 import ProductDetailsHeader from "@/Components/sections/product-details/ProductDetailsHeader";
 import AppLayout from "@/Layouts/AppLayout";
+import { useEffect, useState } from "react";
 
-const ProductDetailsPage = ({ category }) => {
-  let headerTitle = "";
-  if (category === "laminated-bow") {
-    headerTitle = "Laminated Bow";
-  } else if (category === "flat-bow") {
-    headerTitle = "Flat Fiber Bow";
-  } else if (category === "arrows") {
-    headerTitle = "Arrows";
-  } else {
-    headerTitle = "Bow Accessories";
-  }
+const ProductDetailsPage = ({ category, name, id }) => {
+  const [productData, setProductData] = useState();
+
+  // fetch data from database based on product id
+  useEffect(() => {
+    const fetchProductData = async () => {
+      try {
+        const response = await axios.get(`/api/products-details/${id}`);
+        setProductData(response.data);
+      } catch (error) {
+        console.error(
+          "There was an error when fetching products-details data: ",
+          error
+        );
+      }
+    };
+    fetchProductData();
+  }, []);
+
+  console.log(productData);
+
   return (
     <AppLayout>
       <div className="flex flex-col w-full h-72 xl:h-96 md:h-[400px] mx-auto mt-0">
-        <ProductDetailsHeader title={headerTitle} category={category} />
+        <ProductDetailsHeader title={name} category={category} />
       </div>
       <div className="flex flex-col w-[90%] mx-auto my-20 mt-10 ">
         <ProductDetails />
