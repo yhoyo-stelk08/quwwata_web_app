3db6c82a79017d7dac03eaa83ba8689fad332927
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-01 22:18:49 +0800

    feat: rename back CheckoutPage to CartPage.

--- resources/js/Pages/CartPage.jsx
+++ resources/js/Pages/CartPage.jsx
@@ -1,7 +1,8 @@
+import Cart from "@/Components/Cart";
 import AppLayout from "@/Layouts/AppLayout";
 
-export default function CartPage({ product }) {
-  console.log(product);
+export default function CartPage({ orderItems }) {
+  console.log(orderItems);
   return (
     <AppLayout>
       <div className="flex flex-col w-[90%] mx-auto my-16">
@@ -14,7 +15,9 @@ export default function CartPage({ product }) {
         {/* Cart Content */}
         <div className="flex flex-col md:flex-row gap-8 max-w-6xl mx-auto mt-10">
           {/* Product Div */}
-          <div>Product Div</div>
+          <div>
+            <Cart />
+          </div>
           {/* Total Div */}
           <div>Total Div</div>
         </div>
