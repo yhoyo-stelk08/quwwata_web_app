c84172e922a153821a5b4324640df3cf76df1521
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-05 21:33:50 +0800

    feat: add save customer to db function.

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCustomerRequest;
+use App\Models\Customer;
 use App\Models\Products;
 use Illuminate\Http\Request;
 
@@ -20,6 +22,27 @@ class CartController extends Controller
         return inertia('CheckoutPage', [
             'orderItems' => $orderItems,
         ]);
+    }
+
+    public function order(StoreCustomerRequest $request)
+    {
+        // dd($request->all());
+        $validated_data = $request->validated();
+        // dd($validated_data);
+
+        // Detach orderItems from validated data
+        $orderItems = $validated_data['orderItems'];
+        unset($validated_data['orderItems']);
+
+        // Detach remark from validated data
+        $remark = $validated_data['remark'];
+        unset($validated_data['remark']);
+
+        // save customer data
+        $customer = Customer::create($validated_data);
+
+        return inertia('HomePage');
+
     }
 
 }
