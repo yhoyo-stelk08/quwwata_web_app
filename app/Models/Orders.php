103ec5dd16a1ff2194bb8dafaa05270492abd949
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-05 21:49:16 +0800

    feat: make order_number column in order table as uuid type.

--- app/Models/Orders.php
+++ app/Models/Orders.php
@@ -4,12 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Orders extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    // generate uuid for order number
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->order_number)) {
+                $model->order_number = Str::uuid()->toString();
+            }
+        });
+    }
 
     public function customer()
     {
