4ae0ff9912ca3cd9c8de870f77a5b9bb44fd31a8
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-06 14:31:49 +0800

    feat: populate PaypalController

--- app/Http/Controllers/Gateway/PaypalController.php
+++ app/Http/Controllers/Gateway/PaypalController.php
@@ -4,19 +4,64 @@ namespace App\Http\Controllers\Gateway;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Srmklive\PayPal\Services\PayPal as PayPalClient;
 
 class PaypalController extends Controller
 {
     public function payment(Request $request)
     {
+        $totalAmount = $request->input('total');
 
+        $provider = new PayPalClient;
+        $provider->setApiCredentials(config('paypal'));
+        $paypalToken = $provider->getAccessToken();
+
+        $response = $provider->createOrder([
+            "intent" => "CAPTURE",
+            "application_context" => [
+                "return_url" => route('paypal.success'),
+                "cancel_url" => route('paypal.cancel')
+            ],
+            "purchase_units" => [
+                [
+                    "amount" => [
+                        "currency_code" => "USD",
+                        "value" => $totalAmount
+                    ]
+                ]
+            ]
+        ]);
+
+        if (isset($response['id']) && $response['status'] == 'CREATED') {
+            foreach ($response['links'] as $link) {
+                if ($link['rel'] == 'approve') {
+                    return Inertia::location($link['href']);
+                    // return redirect()->away($link['href']);
+                }
+            }
+        } else {
+            return redirect()->route('/')->with('error', 'Payment failed.');
+        }
     }
 
-    public function success()
+    public function success(Request $request)
     {
+        $provider = new PayPalClient;
+        $provider->setApiCredentials(config('paypal'));
+        $provider->getAccessToken();
+
+        $response = $provider->capturePaymentOrder($request['token']);
+
+        if (isset($response['status']) && $response['status'] == 'COMPLETED') {
+            return redirect()->route('/')->with('success', 'Payment successful.');
+        }
+
+        return redirect()->route('/')->with('error', 'Payment failed.');
     }
 
     public function cancel()
     {
+        return redirect()->route('/')->with('error', 'Payment was cancelled.');
     }
 }
