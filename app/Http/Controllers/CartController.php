23719810e4be0a108a8d35774f13be83e21f14a4
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-06 14:37:01 +0800

    feat: handle customer and orders and redirect to paypal.

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,22 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreCustomerRequest;
 use App\Models\Customer;
-use App\Models\Products;
 use App\Models\Orders;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class CartController extends Controller
 {
     public function cart(Request $request)
     {
-        // $product = Products::findMany(array_column($orderItems, 'product_id'));
         return inertia('CartPage');
     }
 
     public function checkout(Request $request)
     {
         $orderItems = $request->input('orderItems');
-        // dd($orderItems);
         return inertia('CheckoutPage', [
             'orderItems' => $orderItems,
         ]);
@@ -27,41 +25,39 @@ class CartController extends Controller
 
     public function order(StoreCustomerRequest $request)
     {
-        // dd($request->all());
         $validated_data = $request->validated();
-        // dd($validated_data);
 
-        // Detach orderItems from validated data
         $orderItems = $validated_data['orderItems'];
         unset($validated_data['orderItems']);
 
-        // Detach remark from validated data
         $remark = $validated_data['remark'];
         unset($validated_data['remark']);
 
-        // save customer data
+        $payment_method = $validated_data['payment_method'];
+        unset($validated_data['payment_method']);
+
+        $totalAmount = $validated_data['totalAmount'];
+        unset($validated_data['totalAmount']);
+
         $customer = Customer::create($validated_data);
 
-        // handle order items
         foreach ($orderItems as $orderItem) {
             $order = new Orders([
                 'customer_id' => $customer->id,
                 'product_id' => $orderItem['product_id'],
                 'quantity' => $orderItem['quantity'],
-                'sub_total' => $orderItem['quantity'] * $orderItem['price'], // Assuming price is per item
-                'discount' => 0, // Set to 0 or calculate if applicable
-                'total' => $orderItem['itemTotal'], // Adjust if discount or other fees are applied
+                'sub_total' => $orderItem['quantity'] * $orderItem['price'],
+                'discount' => 0,
+                'total' => $orderItem['itemTotal'],
                 'status' => 'pending',
                 'order_notes' => $remark,
                 'draw_weight' => $orderItem['draw_weight'],
                 'arrow_pass' => $orderItem['arrow_pass'],
+                'payment_method' => $payment_method,
             ]);
-            // dd($order);
             $order->save();
         }
 
-        return inertia('HomePage');
-
+        return redirect()->route('paypal.payment', ['total' => $totalAmount]);
     }
-
 }
