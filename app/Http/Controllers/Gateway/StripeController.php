b4dfa1e441942c01bba0301550d918c696d2d4ed
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-09 00:40:02 +0800

    feat: create skeleton method.

--- app/Http/Controllers/Gateway/StripeController.php
+++ app/Http/Controllers/Gateway/StripeController.php
@@ -7,5 +7,15 @@ use Illuminate\Http\Request;
 
 class StripeController extends Controller
 {
-    //
+    public function payment(Request $request)
+    {
+        dd($request->all());
+    }
+
+    public function success()
+    {
+    }
+    public function cancel()
+    {
+    }
 }
