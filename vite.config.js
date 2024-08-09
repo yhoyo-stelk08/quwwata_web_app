3701c9a2897fb01fee532787790e2e0dd63c657d
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-09 17:49:59 +0800

    feat: remove the define process.env

--- vite.config.js
+++ vite.config.js
@@ -27,9 +27,4 @@ export default defineConfig({
     },
     chunkSizeWarningLimit: 1000, // Adjust the chunk size warning limit if needed
   },
-  define: {
-    "process.env": {
-      STRIPE_PUBLIC_KEY: JSON.stringify(process.env.STRIPE_PUBLIC_KEY),
-    },
-  },
 });
