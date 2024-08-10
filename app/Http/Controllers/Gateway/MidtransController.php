c09fcaeec7e0dbcb9c15328a4afcf75a58e85d1c
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-10 23:56:32 +0800

    feat: finish the MidtransController logic.

--- app/Http/Controllers/Gateway/MidtransController.php
+++ app/Http/Controllers/Gateway/MidtransController.php
@@ -8,6 +8,8 @@ use Midtrans\Config;
 use Midtrans\Snap;
 use App\Models\Order;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\PaymentSuccessMail;
 
 class MidtransController extends Controller
 {
@@ -25,16 +27,21 @@ class MidtransController extends Controller
         // Fetch the order from the database
         $order = Order::with('orderItems')->find($request->order_id);
 
+        // get usd to idr convert value
+        $convert_value = $request->convert_value;
+
         if (!$order) {
             // Todo create page order not found
             return redirect()->route('/')->with('error', 'Order not found.');
         }
 
+        // Generate a new unique transaction id by appending a timestamp or a unique identifier
+        $transaction_id = $order->id . '-' . uniqid();
 
         // Prepare the transaction details
         $params = [
             'transaction_details' => [
-                'order_id' => $order->id,
+                'order_id' => $transaction_id,
                 'gross_amount' => $order->orderItems->sum('total'),
             ],
             'customer_details' => [
@@ -43,10 +50,10 @@ class MidtransController extends Controller
                 'email' => $order->customer->email,
                 'phone' => $order->customer->phone_number,
             ],
-            'item_details' => $order->orderItems->map(function ($item) {
+            'item_details' => $order->orderItems->map(function ($item) use ($convert_value) {
                 return [
                     'id' => $item->id,
-                    'price' => $item->total,
+                    'price' => $item->total * $convert_value,
                     'quantity' => $item->quantity,
                     'name' => $item->product->name,
                 ];
@@ -56,13 +63,12 @@ class MidtransController extends Controller
         // Create Snap transaction
         $snapToken = Snap::getSnapToken($params);
 
-        // dd($snapToken);
-
-        // Render the Midtrans Snap payment page
-        // return Inertia::render('MidtransPaymentPage', ['snapToken' => $snapToken]);
-
         // Redirect to Midtrans payment page
         $paymentUrl = "https://app.sandbox.midtrans.com/snap/v2/vtweb/{$snapToken}";
+
+        // save  snap token to order table  as transaction id
+        $order->transaction_id = $transaction_id;
+        $order->save();
 
         return Inertia::location($paymentUrl);
     }
@@ -73,29 +79,37 @@ class MidtransController extends Controller
         $orderId = $request->input('order_id');
 
         // Find the order by ID and update it with the transaction ID and status
-        $order = Order::find($orderId);
+        $order = Order::where('transaction_id', $orderId)->first();
         if ($order) {
             $order->status = 'completed';
-            $order->transaction_id = $request->input('transaction_id');
             $order->save();
 
-            // Redirect to the receipt page or display a success message
-            return redirect()->route('receipt', ['order_id' => $orderId]);
+            $transactionId = $order->transaction_id;
+            $amount = $order->getTotalAttribute();
+
+            // Send the payment success email to the customer
+            Mail::to($order->customer->email)->send(new PaymentSuccessMail($order, $transactionId, $amount));
+
+            return Inertia::render('ReceiptPage', [
+                'transactionId' => $transactionId,
+                'amount' => $amount,
+                'order' => $order
+            ]);
         }
 
-        return redirect()->route('checkout')->with('error', 'Payment failed.');
+        return Inertia::location('checkout')->with('error', 'Payment failed.');
     }
 
     public function cancel(Request $request)
     {
-        $orderId = $request->query('order_id');
-        $order = Order::find($orderId);
+        $transaction_id = $request->query('order_id');
+        $order = Order::where('transaction_id', $transaction_id)->first();
 
         if ($order) {
             $order->status = 'declined';
             $order->save();
         }
 
-        return redirect()->route('checkout')->with('error', 'Payment was cancelled.');
+        return Inertia::location('checkout')->with('error', 'Payment failed.');
     }
 }
