71ba81af4e5ee629d8f775867d7f788fb5ae94b5
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-12 21:30:34 +0800

    feat: adding function to display order

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\OrderResource;
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function index(Request $request)
+    {
+        \Log::debug('Entering Orders Index Method');
+
+        $query = Order::with(['customer', 'orderItems.product']);
+
+        // Add sorting
+        if ($request->has('sort_by') && $request->has('sort_direction')) {
+            $query->orderBy($request->get('sort_by'), $request->get('sort_direction'));
+        } else {
+            // Default sorting
+            $query->orderBy('updated_at');
+        }
+
+        // paginate the result
+        $orders = $query->paginate(10);
+        \Log::debug('Orders Query Result', $orders->toArray());
+
+        // Transform the orders data using OrderResource
+        $orderData = OrderResource::collection($orders);
+        \Log::debug('Transformed Orders Data', $orderData->toArray($request));
+
+        return inertia('Orders/Index', [
+            'orderData' => $orderData,
+            'search' => $request->search ?? "",
+            'sort_by' => $request->sort_by ?? "",
+            'sort_direction' => $request->sort_direction ?? "",
+        ]);
+    }
+}
