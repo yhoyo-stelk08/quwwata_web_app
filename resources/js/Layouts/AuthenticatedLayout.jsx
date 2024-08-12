90c6c41bf44d5ca56f23a20ab32a9ad4851c9faa
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-12 21:29:44 +0800

    feat: adding link to manage order.

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -46,6 +46,14 @@ export default function Authenticated({ user, header, children }) {
                   Products
                 </NavLink>
               </div>
+              <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <NavLink
+                  href={route("order.index")}
+                  active={route().current("order.index")}
+                >
+                  Order
+                </NavLink>
+              </div>
             </div>
 
             <div className="hidden sm:flex sm:items-center sm:ms-6">
