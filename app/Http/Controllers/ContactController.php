b9982c856da90ea80156ed65ca25a868ace5907d
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-13 09:49:02 +0800

    feat: implement logic of index function

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ContactRequest;
+use App\Http\Resources\ContactResource;
 use App\Models\Contact;
 use Illuminate\Http\Request;
 
@@ -10,6 +11,30 @@ class ContactController extends Controller
 {
     public function index(Request $request)
     {
+        \Log::debug('Entering Contacts Index Method');
+
+        $query = Contact::search($request);
+
+        // Add sorting
+        if ($request->has('sort_by') && $request->has('sort_direction')) {
+            $query->orderBy($request->get('sort_by'), $request->get('sort_direction'));
+        } else {
+            // Default sorting
+            $query->orderByDesc('updated_at');
+        }
+
+        $contacts = $query->paginate(10);
+
+        $contactsData = ContactResource::collection($contacts);
+
+        \Log::info('Contacts Data', ['Contacts Data' => $contactsData]);
+
+        return inertia('Contacts/Index', [
+            'contactsData' => $contactsData,
+            'search' => $request->search ?? "",
+            'sort_by' => $request->sort_by ?? "",
+            'sort_direction' => $request->sort_direction ?? "",
+        ]);
     }
 
     public function store(ContactRequest $request)
