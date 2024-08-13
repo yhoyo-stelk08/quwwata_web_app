1c8e508c78348eef8fa4654c330b14acc0bb6c2c
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-13 13:25:29 +0800

    feat: add sorting logic

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -21,7 +21,27 @@ class OrderController extends Controller
 
         // Add sorting
         if ($request->has('sort_by') && $request->has('sort_direction')) {
-            $query->orderBy($request->get('sort_by'), $request->get('sort_direction'));
+            if ($request->get('sort_by') == 'customer_name') {
+                $query->join('customers', 'orders.customer_id', '=', 'customers.id')
+                    ->orderBy('customers.first_name', $request->get('sort_direction'))
+                    ->select('orders.*');
+            } else if ($request->get('sort_by') == 'customer_phone') {
+                $query->join('customers', 'orders.customer_id', '=', 'customers.id')
+                    ->orderBy('customers.phone_number', $request->get('sort_direction'))
+                    ->select('orders.*');
+            } else if ($request->get('sort_by') == 'customer_email') {
+                $query->join('customers', 'orders.customer_id', '=', 'customers.id')
+                    ->orderBy('customers.email', $request->get('sort_direction'))
+                    ->select('orders.*');
+            } else if ($request->get('sort_by') == 'total_payment') {
+                $query->join('order_items', 'orders.id', '=', 'order_items.id')
+                    ->orderBy('order_items.total', $request->get('sort_direction'))
+                    ->select('orders.*');
+            } else if ($request->get('sort_by') == 'payment_status') {
+                $query->orderBy('status', $request->get('sort_direction'));
+            } else {
+                $query->orderBy($request->get('sort_by'), $request->get('sort_direction'));
+            }
         } else {
             // Default sorting
             $query->orderBy('updated_at');
