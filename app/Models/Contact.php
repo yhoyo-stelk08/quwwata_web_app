39a1291ab8179b73156d3fb2437d5529e5161fa1
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-13 09:41:12 +0800

    feat: add scopeSearch function to Contact models.

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,4 +13,15 @@ class Contact extends Model
     protected $table = 'contacts';
 
     protected $guarded = [];
+
+    public function scopeSearch(Builder $query, $search): Builder
+    {
+        if ($search) {
+            return $query->where('email', 'like', '%' . $search . '%')
+                ->orWhere('contact_number', 'like', '%' . $search . '%')
+                ->orWhere('message', 'like', '%' . $search . '%');
+        }
+
+        return $query;
+    }
 }
