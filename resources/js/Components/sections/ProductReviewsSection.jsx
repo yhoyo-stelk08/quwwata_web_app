c5a42905544ba5def5220330d1a42ce6a870f151
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-15 10:46:34 +0800

    feat: create ProductDetailsReview components.

--- resources/js/Components/sections/ProductReviewsSection.jsx
+++ resources/js/Components/sections/ProductReviewsSection.jsx
@@ -1,3 +1,5 @@
+import ProductDetailsReview from "./product-details/ProductDetailsReview";
+
 const reviewsData = [];
 
 const ProductReviewsSection = () => {
@@ -5,17 +7,17 @@ const ProductReviewsSection = () => {
     <div className="flex flex-col justify-center items-center w-full">
       <div className="flex flex-col justify-center items-center w-full h-auto py-8 mt-10 border-b">
         {/* Header */}
-        <h1 className="text-4xl text-slate-200 font-quicksand font-semibold italic tracking-wider">
+        <h1 className="text-xl md:text-4xl text-slate-200 font-quicksand font-semibold italic tracking-wider">
           REVIEWS
         </h1>
       </div>
       <div className="w-full h-auto flex flex-col md:flex-row justify-center items-center border-b p-8">
         {/* Reviews */}
         {reviewsData.length > 0 ? (
-          ""
+          <ProductDetailsReview />
         ) : (
           <div className="flex flex-col items-center justify-center">
-            <h3 className="text-2xl text-slate-200 font-sans  tracking-wider">
+            <h3 className="text-xl md:text-2xl text-slate-200 font-sans tracking-wider text-center">
               No Reviews Yet, Write One ?
             </h3>
             <button className=" text-slate-200 px-8 py-2 rounded-full mt-4 bg-slate-700 hover:bg-slate-200 hover:text-slate-800  duration-300 ">
---
+++ resources/js/Components/sections/product-details/ProductDetailsReview.jsx
@@ -0,0 +1,4 @@
+const ProductDetailsReview = () => {
+  return <div>ProductDetailsReview</div>;
+};
+export default ProductDetailsReview;
