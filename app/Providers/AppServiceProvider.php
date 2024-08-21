187690d64bf19fac84aa30d296aa4377019a461f
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-21 10:58:22 +0800

    feat: bind GalleryRepository, GalleryEloquent and GalleryService in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\GalleryService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+
+        // Bind the GalleryRepository and GalleryEloquent
+        $this->app->bind(
+            \App\Repositories\GalleryRepository::class,
+            \App\Repositories\GalleryEloquent::class,
+        );
+
+        // Bind the GalleryService
+        $this->app->bind(GalleryService::class, function ($app) {
+            return new GalleryService();
+        });
     }
 
     /**
